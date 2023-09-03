53635c5e431b9426100f18fa962dd1fa3bdb1711
Nitesh Verma <niteshavagabond@gmail.com>
2023-09-03 20:59:43 +0530

    added login and signup pages

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request, redirect
 
 
 auth = Blueprint('auth',__name__)
@@ -7,9 +7,25 @@ auth = Blueprint('auth',__name__)
 def signup():
     return render_template('signup.html')
 
+@auth.route('/signup',methods=['POST'])
+def signup_post():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    return redirect(url_for('auth.login'))
+
+
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    return redirect(url_for('main.profile'))
 
 @auth.route('/logout')
 def logout():
