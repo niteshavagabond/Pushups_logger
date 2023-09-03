0c8cc681c472506d56e29fde87df3d2cb963a960
Nitesh Verma <niteshavagabond@gmail.com>
2023-09-03 19:45:52 +0530

    editing homepage

--- app/auth.py
+++ app/auth.py
@@ -5,11 +5,11 @@ auth = Blueprint('auth',__name__)
 
 @auth.route('/signup')
 def signup():
-    return render_template('signup')
+    return render_template('signup.html')
 
 @auth.route('/login')
 def login():
-    return render_template('login')
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
